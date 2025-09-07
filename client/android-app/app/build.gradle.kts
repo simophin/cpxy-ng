5d78e65527ff3edef9794df8a2383b3b04cc20ab
Fanchao Liu <273191+simophin@users.noreply.github.com>
2025-09-08 07:32:11 +0800

    Update build.gradle.kts

--- client/android-app/app/build.gradle.kts
+++ client/android-app/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.fanchao.cpxy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -32,7 +32,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
